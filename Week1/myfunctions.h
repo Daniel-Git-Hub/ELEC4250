9110ecbf69461c5fe8d6ab1d20e5caa58f89db39
Daniel-Git-Hub <daniel.watson.2001@gmail.com>
2021-07-30 10:42:10 +1000

    Create myfunctions.h

---
+++ Week1/myfunctions.h
@@ -0,0 +1,15 @@
+#ifndef MYFUNCTIONS
+#define MYFUNCTIONS
+
+
+// this function adds matrices A & B and returns the output as result
+void m_add(void *, void *, void *, int );
+
+// this function multiplies matrices A & B and returns the output as result
+void m_multiply(void *, void *, void *, int);
+
+void m_parse(char *, void *, int);
+
+void m_print(void *, int);
+
+#endif
